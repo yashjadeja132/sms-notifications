cfc6c43f533db28d50581b43b82276fd22382ec5
ShivamShukla77 <shivamshukla@keybrainstech.com>
2024-12-18 10:48:42 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const sendSlackNotification = require("./slack");
+const app = express();
+const port = 3000;
+
+const email = "email@example.com";
+const messageText =
+  "🚀 SEO Validation Completed: All links passed successfully!";
+
+sendSlackNotification(email, messageText);
+
+app.get("/", (req, res) => {
+  res.send("Hello, Node.js with Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ slack.js
@@ -0,0 +1,44 @@
+const { WebClient } = require("@slack/web-api");
+
+const token = process.env.TOKEN_KEY;
+const web = new WebClient(token);
+
+async function getUserIdByEmail(email) {
+  try {
+    const result = await web.users.lookupByEmail({ email });
+
+    if (result.user) {
+      console.log(`User ID for ${email}: ${result.user.id}`);
+      return result.user.id;
+    } else {
+      console.log(`User not found for email: ${email}`);
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching user by email:", error);
+    return null;
+  }
+}
+
+async function sendSlackNotificationToUser(email, messageText) {
+  const userId = await getUserIdByEmail(email);
+
+  if (userId) {
+    try {
+      const result = await web.chat.postMessage({
+        channel: userId,
+        text: messageText,
+      });
+
+      console.log("Message sent successfully!", result.ts);
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  } else {
+    console.log(
+      `User with email ${email} not found in the workspace. Sending email...`
+    );
+  }
+}
+
+module.exports = sendSlackNotificationToUser;
